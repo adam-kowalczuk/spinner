892e3d54dfc109ca1555976c3f706ef09ce447f0
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-15 12:25:35 -0700

    Made spinner a function, and added another parameter to show what operation is being handled while it spins.

--- spinner2.js
+++ spinner2.js
@@ -1,13 +1,17 @@
 process.stdout.write('hello from spinner2.js... \rheyyy\n');
 
-const animation = "|/-\\|/-\\|";
-let timer = 100;
-for (const char of animation) {
-  setTimeout(() => {
-    process.stdout.write(`\r${char}   `);
-  }, timer);
-  timer += 200;
-}
+const spinner2 = (action) => {
+  const animation = "|/-\\|/-\\|";
+  let timer = 100;
+  for (const char of animation) {
+    setTimeout(() => {
+      process.stdout.write(`\r${char}   ${action}`);
+    }, timer);
+    timer += 200;
+  }
+};
+
+spinner2("Thinking...");
 
 
 
